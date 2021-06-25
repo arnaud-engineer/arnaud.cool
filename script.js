2b92e68a6f3b4eaadb161b1af60ee0f94ce9b586
arnaud-engineer <f@nanaud.fr>
2021-06-25 13:01:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -364,16 +364,16 @@ document.addEventListener('DOMContentLoaded', function(event)
 	initYT();
 
 	// GitHub Pages awfull fix attempt
-	document.getElementById("myOffice").style.height = "0px";
-	document.getElementById("myOffice").style.height = "var(--frameHeight)";
+	document.getElementById("myOffice").style.position = "relative";
+	document.getElementById("myOffice").style.position = "absolute";
 	//document.getElementById("myOffice").style.overflow = "auto";
 	//document.getElementById("myOffice").style.overflow = "hidden";
 
 	setTimeout(() => {
 		//document.getElementById("myOffice").style.overflow = "auto";
 		//document.getElementById("myOffice").style.overflow = "hidden";
-		document.getElementById("myOffice").style.height = "0px";
-		document.getElementById("myOffice").style.height = "var(--frameHeight)";
+		document.getElementById("myOffice").style.position = "relative";
+		document.getElementById("myOffice").style.position = "absolute";
 	}, 10000);
 
 	//onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)};
